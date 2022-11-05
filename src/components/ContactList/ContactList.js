a3ed826fa7d48e97c8ed78cf38eeaf71b4de48ad
Alexandra-Makarenko <s850272@gmail.com>
2022-11-05 18:50:43 +0200

    ready

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -10,12 +10,8 @@ export const ContactList = () => {
 
   const contacts = useSelector(state => state.contacts);
   const filter = useSelector(state => state.filter);
-  // console.log(Object.values(contacts))
 
-  // const visibleContacts = (Object.values(contacts)).filter(contact =>
-  //       console.log(contact.name.toLowerCase()),
-  // );
-   const visibleContacts = Object.values(contacts).filter(contact =>
+   const visibleContacts = Object.values(contacts.items).filter(contact =>
         contact?.name?.toLowerCase()?.includes(filter),
       );
   const dispatch = useDispatch();
@@ -28,8 +24,8 @@ export const ContactList = () => {
                 {visibleContacts.map((contact) => (
                     <Item key={contact.id}
                     > {<ListItem
-            contact={contact}/>}
-                        <Button
+                      contact={contact}/>}
+         <Button
           type="button"
           onClick={() => onDeleteContact(contact.id)}
         >
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,25 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "nanoid";
 
-const contactsInitialState = [
+const contactsInitialState = {items:[
     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ];
-//   console.log(contactsInitialState)
+  ]};
 
 const contactsSlice = createSlice({
     name: "contacts",
     initialState: contactsInitialState,
     reducers: {
         addContact: {
-            reducer(state, action) {
-                console.log(state)
-                // state =[JSON.stringify(state),action.payload];
-                // state = JSON.stringify(state).concat(JSON.stringify(action.payload));
-                console.log(state.contacts)
-            },
+            reducer(state, action) {                
+                state.items.push(action.payload)
+                        },
             
             prepare(name,number) {
                 return {
@@ -32,8 +28,8 @@ const contactsSlice = createSlice({
             },
         },
         deleteContact(state, action) {
-      const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+      const index = state.items.findIndex(contact => contact.id === action.payload);
+      state.items.splice(index, 1);
         },
         
     }
